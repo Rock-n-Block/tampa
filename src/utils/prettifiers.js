23d1885877a5311c47b074900d838e942ae9bed4
Boris Kov <bkovesh@gmail.com>
2020-12-17 19:44:12 +0300

    date format for lottery pool

--- src/utils/prettifiers.js
+++ src/utils/prettifiers.js
@@ -1,7 +1,7 @@
 import { format } from 'date-fns';
 
 
-export const dateFormat = (date = '00.00.0000') => {
+export const dateFormat = (date) => {
     const dateArray = format(new Date(date * 1000), 'dd.MMM.Y').split('.');
     dateArray[1] = dateArray[1].toUpperCase();
     return dateArray.join('.');
