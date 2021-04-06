d981c9e16acfbf291e5790282d8a7d8f436bd796
Boris Kov <bkovesh@gmail.com>
2021-04-06 11:49:06 +0300

    Text changes

--- src/components/StakeInfo/index.js
+++ src/components/StakeInfo/index.js
@@ -48,7 +48,7 @@ const StakeInfo = ({ totalStaked, totalShares, totalInterests, totalDividents, i
                         <RowItemTooltip tooltipText={totalDividents} parent="s-info__content">{totalDividents}</RowItemTooltip>
                     </div>
                     <div className="s-info__item-subhead">Total eth dividends
-                            <QuestionTooltip isDarkTheme={isDarkTheme} parent="s-info__content" tooltipText="One of the benefits of Staking J is ETH dividends. At the end of each day, a ETH dividends pool will be calculated and allocated to all the open stakes based on their stake amount.<br><br> The ETH dividends pool comes from the total daily entry of the auction lobby the only way to receive ETH Dividends is having open stakes.<br><br> After you exit your stakes you will receive your ETH dividends." />
+                            <QuestionTooltip isDarkTheme={isDarkTheme} parent="s-info__content" tooltipText="One of the benefits of Staking Jackpot tokens is the Ethereum dividends you receive. At the end of each day, an ETH dividends pool will be calculated and allocated to all the open stakes based on their stake amount, stake length, and stake start date.<br><br> The ETH dividends pool comes from the total daily entry of the auction lobby. The only way to receive ETH Dividends is to stake your Jackpot tokens.<br><br> Not only will you get ETH dividends when you stake, but you will also be rewarded with more Jackpot tokens every day.  You can collect both the ETH and Jackpot rewards without penalty at the end of your stake, all you have to pay is your ETH gas fee.<br><br> After you exit your stakes you will receive your ETH dividends." />
                     </div>
                 </div>
                 <div className="s-info__item">
@@ -57,7 +57,7 @@ const StakeInfo = ({ totalStaked, totalShares, totalInterests, totalDividents, i
                     </div>
                     <div className="s-info__item-subhead">{isActiveStakes ? 'Total interest' : 'total bonus shares'}
                         {isActiveStakes &&
-                            <QuestionTooltip isDarkTheme={isDarkTheme} parent="s-info__content" tooltipText="[J supply * 4% inflation per year/ 364], this daily pool will be distributed between all the Stakers as their stake interest." />
+                            <QuestionTooltip isDarkTheme={isDarkTheme} parent="s-info__content" tooltipText={`Please note that in addition to your ETH dividends, Jackpot supply has a 12% inflation per year. This daily pool will be distributed between all the Stakers as their stake interest in Jackpot. Although Jackpot has this "inflation", it is possible that Jackpot tokens overall have a deflationary outcome because there are various deflationary aspects that will be put in place in regards to the exchange of the Jackpot tokens and the use cases.  The goal is that there will be fewer and fewer Jackpot tokens every year, while Jackpot stakers continue to earn more Jackpot tokens over time.  This design is meant to keep the price of Jackpot going up, and the overall supply going down.`} />
                         }
                     </div>
                 </div>
